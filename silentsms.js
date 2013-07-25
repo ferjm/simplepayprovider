a4f13c55b1fd112bdebd77845f56625d6af78e13
Fernando Jiménez <ferjmoreno@gmail.com>
2013-07-25 20:30:21 +0200

    Silent SMS test page

---
+++ silentsms.js
@@ -0,0 +1,20 @@
+function sendSilentSms() {
+  var number = document.getElementById('sendNumber').value;
+  var sendResult = document.getElementById('sendResult');
+  var req = mozPaymentProvider.sendSilentSms(number, 'Silence!');
+  req.onsuccess = function() {
+    sendResult.textContent = 'Successfully sent silent sms! \o/';
+  };
+  req.onerror = function() {
+    sendResult.textContent = 'Damn it! ' + req.error.name;
+  };
+}
+
+function observeSilentSms() {
+  var number = document.getElementById('observeNumber').value;
+  var observeResult = deocument.getElementById('observeResult');
+  mozPaymentProvider.observeSilentSms(var number, function(message) {
+    observeResult.textContent = 'Got silence SMS from ' + message.sender +
+                                ' -> ' + message.body;
+  });
+}
